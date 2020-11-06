c1787c121cd7a728435917f0f911d9f26f2873cd
jose <jota.rf.88@gmail.com>
2020-11-06 19:22:55 -0400

    Creados todos los insert de los estados

--- Inserts/INSERTS estados.sql
+++ Inserts/INSERTS estados.sql
@@ -1,35 +1,55 @@
 /****** ESTADOS ******/
 
 INSERT INTO estados (id, datos_ubicacion)
-VALUES (id_estado_sec.NEXTVAL, datos_ubicacion("Distrito Capital",10.47639,-66.98333));
+VALUES (id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Amazonas'),datos_ubicacion.validar_latitud(3.5),datos_ubicacion.validar_longitud(-66)));
+INSERT INTO estados (id, datos_ubicacion)
+VALUES (id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Anzoátegui'),datos_ubicacion.validar_latitud(9),datos_ubicacion.validar_longitud(-64.33334)));
+INSERT INTO estados (id, datos_ubicacion)
+VALUES (id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Apure'),datos_ubicacion.validar_latitud(7.16667),datos_ubicacion.validar_longitud(-68.83334)));
+INSERT INTO estados (id, datos_ubicacion)
+VALUES (id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Distrito Capital'),datos_ubicacion.validar_latitud(10.47639),datos_ubicacion.validar_longitud(-66.98333)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Carabobo',10.16667,-68.08334));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Carabobo'),datos_ubicacion.validar_latitud(10.16667),datos_ubicacion.validar_longitud(-68.08334)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Miranda',10.25,-66.41666));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Miranda'),datos_ubicacion.validar_latitud(10.25),datos_ubicacion.validar_longitud(-66.41666)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Zulia',10,-72.16666));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Zulia'),datos_ubicacion.validar_latitud(10),datos_ubicacion.validar_longitud(-72.16666)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Aragua',10,-67.16666));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Aragua'),datos_ubicacion.validar_latitud(10),datos_ubicacion.validar_longitud(-67.16666)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Barinas',8.16667,-69.83334));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Barinas'),datos_ubicacion.validar_latitud(8.16667),datos_ubicacion.validar_longitud(-69.83334)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Cojedes',9.33333,-68.33334));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Cojedes'),datos_ubicacion.validar_latitud(9.33333),datos_ubicacion.validar_longitud(-68.33334)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Lara',10.16667,-69.83334));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Lara'),datos_ubicacion.validar_latitud(10.16667),datos_ubicacion.validar_longitud(-69.83334)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Monagas',9.33333,-63));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Monagas'),datos_ubicacion.validar_latitud(9.33333),datos_ubicacion.validar_longitud(-63)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Sucre',10.41667,-63.5));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Sucre'),datos_ubicacion.validar_latitud(10.41667),datos_ubicacion.validar_longitud(-63.5)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Trujillo',9.41667,-70.5));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Trujillo'),datos_ubicacion.validar_latitud(9.41667),datos_ubicacion.validar_longitud(-70.5)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('La Guaira',10.56667,-66.9));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('La Guaira'),datos_ubicacion.validar_latitud(10.56667),datos_ubicacion.validar_longitud(-66.9)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Yaracuy',10.333333,-68.750000));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Yaracuy'),datos_ubicacion.validar_latitud(10.333333),datos_ubicacion.validar_longitud(-68.750000)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Bolívar',6.33333,-63.5));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Bolívar'),datos_ubicacion.validar_latitud(6.33333),datos_ubicacion.validar_longitud(-63.5)));
 INSERT INTO estados (id, datos_ubicacion) VALUES
-(id_estado_sec.NEXTVAL, datos_ubicacion('Táchira',7.83333,-72.08334));
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Táchira'),datos_ubicacion.validar_latitud(7.83333),datos_ubicacion.validar_longitud(-72.08334)));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Delta Amacuro'),datos_ubicacion.validar_latitud(8.750000),datos_ubicacion.validar_longitud(-61.333332)));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Falcón'),datos_ubicacion.validar_latitud(11),datos_ubicacion.validar_longitud(-69.83334)));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Guárico'),datos_ubicacion.validar_latitud(8.66667),datos_ubicacion.validar_longitud(-66.58334)));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Mérida'),datos_ubicacion.validar_latitud(8.5),datos_ubicacion.validar_longitud(-71.16666)));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Nueva Esparta'),datos_ubicacion.validar_latitud(11),datos_ubicacion.validar_longitud(-64)));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion(datos_ubicacion.validar_nomber('Portuguesa'),datos_ubicacion.validar_latitud(9.16667),datos_ubicacion.validar_longitud(-69.25)));
+
+
 
 
 
