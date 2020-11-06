c477d353de60674358c3404f9619c62ef1a0ac66
jose <jota.rf.88@gmail.com>
2020-11-06 16:42:55 -0400

    Inserts de estados creados

---
+++ Inserts/INSERTS estados.sql
@@ -0,0 +1,38 @@
+/****** ESTADOS ******/
+
+INSERT INTO estados (id, datos_ubicacion)
+VALUES (id_estado_sec.NEXTVAL, datos_ubicacion("Distrito Capital",10.47639,-66.98333));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Carabobo',10.16667,-68.08334));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Miranda',10.25,-66.41666));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Zulia',10,-72.16666));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Aragua',10,-67.16666));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Barinas',8.16667,-69.83334));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Cojedes',9.33333,-68.33334));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Lara',10.16667,-69.83334));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Monagas',9.33333,-63));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Sucre',10.41667,-63.5));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Trujillo',9.41667,-70.5));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('La Guaira',10.56667,-66.9));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Yaracuy',10.333333,-68.750000));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Bolívar',6.33333,-63.5));
+INSERT INTO estados (id, datos_ubicacion) VALUES
+(id_estado_sec.NEXTVAL, datos_ubicacion('Táchira',7.83333,-72.08334));
+
+
+
+
+
+
