5ead9120456f0253e86a83d44dfd9ada99fa9fe3
jose <jota.rf.88@gmail.com>
2020-11-28 20:28:44 -0400

    modulo transporte arreglado

--- Modulos/modulo_transportes.sql
+++ Modulos/modulo_transportes.sql
@@ -12,6 +12,7 @@ CREATE OR REPLACE PROCEDURE crear_transportes IS
     zona_transporte NUMBER;
     indice_zona NUMBER;
     probabilidad_transporte NUMBER;
+    nombre_estado estados.datos_ubicacion.nombre%type;
     BEGIN
         probabilidad_transporte := 0;
         transportes_a_insertar := 0;
@@ -27,84 +28,92 @@ CREATE OR REPLACE PROCEDURE crear_transportes IS
         FOR proveedor IN (SELECT * FROM proveedores p)
         LOOP
 
-            transportes_a_insertar := CEIL(DBMS_RANDOM.VALUE(7,12));
-            DBMS_OUTPUT.PUT_LINE('Insertando '||transportes_a_insertar||' transportes para el proveedor: '||proveedor.datos_empresa.nombre);
+                FOR crear_estado IN (SELECT DISTINCT(zp.id_estado) id FROM zonas_proveedores zp WHERE proveedor.ID = zp.ID_PROVEEDOR)
+                LOOP
 
-            FOR i IN 1..transportes_a_insertar
-            LOOP
-                --Condicional para determinar el tipo de transporte
-                probabilidad_transporte := CEIL(DBMS_RANDOM.VALUE(0,4));
-                IF probabilidad_transporte = 1 THEN
-                    tipo_transporte := 'bic';
-                ELSIF probabilidad_transporte = 2 THEN
-                    tipo_transporte := 'mot';
-                ELSIF probabilidad_transporte = 3 THEN
-                    tipo_transporte := 'cam';
-                ELSE
-                    tipo_transporte := 'car';
-                END IF;
+                    transportes_a_insertar := CEIL(DBMS_RANDOM.VALUE(7,12));
 
-                --Creación de la placa del transporte
-                IF tipo_transporte != 'bic' THEN
+                    SELECT e.datos_ubicacion.nombre INTO nombre_estado FROM estados e WHERE e.id = crear_estado.id;
+                    DBMS_OUTPUT.PUT_LINE('Insertando '||transportes_a_insertar||' transportes para el proveedor: '||proveedor.datos_empresa.nombre || ' en el estado: ' || nombre_estado);
+
+                    FOR i IN 1..transportes_a_insertar
 
                     LOOP
-                        placa := SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26),1)||SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26) ,1)
-                        ||SUBSTR(numeros_placa,DBMS_RANDOM.VALUE(1,10) ,1)||SUBSTR(numeros_placa,DBMS_RANDOM.VALUE(1,10),1)
-                        ||SUBSTR(numeros_placa, DBMS_RANDOM.VALUE(1,10) ,1)
-                        ||SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26),1)||SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26),1);
-
-
-                        SELECT COUNT(*) INTO existe_placa
-                        FROM transportes
-                        WHERE numero_placa = placa;
-
-                        EXIT WHEN existe_placa = 0;
-
-                    END LOOP;
-                END IF;
-
-                --Condicional para determinar estatus del transporte
-                IF CEIL(DBMS_RANDOM.VALUE(0,10)) <= 2 THEN
-                    estatus := 'd';
-                ELSE
-                    estatus := 'f';
-                END IF;
-
-                --Ciclo para determinar la zona de ubicación del transporte
-
-                SELECT COUNT(*) INTO cantidad_zonas_proveedor
-                FROM zonas_proveedores zp
-                WHERE proveedor.ID=zp.ID_PROVEEDOR;
-
-                zona_transporte := CEIL(DBMS_RANDOM.VALUE(0,cantidad_zonas_proveedor));
-
-                indice_zona := 1;
-
-                FOR crear_zona IN (SELECT zp.ID_ZONA id_zona, zp.ID_MUNICIPIO id_municipio, zp.ID_ESTADO id_estado
-                FROM zonas_proveedores zp
-                WHERE proveedor.ID=zp.ID_PROVEEDOR
-                ORDER BY DBMS_RANDOM.RANDOM())
-
-                LOOP
-                    --Creacion del transporte
-                    IF indice_zona = zona_transporte THEN
-
-                        IF tipo_transporte = 'bic' THEN
-                            INSERT INTO transportes (id, id_proveedor, id_estado, id_municipio, id_zona, tipo, estatus)
-                            VALUES (id_transporte_sec.nextval, proveedor.ID, crear_zona.ID_ESTADO, crear_zona.ID_MUNICIPIO, crear_zona.ID_ZONA, tipo_transporte, estatus);
+                        --Condicional para determinar el tipo de transporte
+                        probabilidad_transporte := CEIL(DBMS_RANDOM.VALUE(0,4));
+                        IF probabilidad_transporte = 1 THEN
+                            tipo_transporte := 'bic';
+                        ELSIF probabilidad_transporte = 2 THEN
+                            tipo_transporte := 'mot';
+                        ELSIF probabilidad_transporte = 3 THEN
+                            tipo_transporte := 'cam';
                         ELSE
-                            INSERT INTO transportes (id, id_proveedor, id_estado, id_municipio, id_zona, tipo, estatus, numero_placa)
-                            VALUES (id_transporte_sec.nextval, proveedor.ID, crear_zona.ID_ESTADO, crear_zona.ID_MUNICIPIO, crear_zona.ID_ZONA, tipo_transporte, estatus, placa);
-
+                            tipo_transporte := 'car';
                         END IF;
 
-                    END IF;
+                        --Creación de la placa del transporte
+                        IF tipo_transporte != 'bic' THEN
 
-                    indice_zona := indice_zona + 1;
+                            LOOP
+                                placa := SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26),1)||SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26) ,1)
+                                ||SUBSTR(numeros_placa,DBMS_RANDOM.VALUE(1,10) ,1)||SUBSTR(numeros_placa,DBMS_RANDOM.VALUE(1,10),1)
+                                ||SUBSTR(numeros_placa, DBMS_RANDOM.VALUE(1,10) ,1)
+                                ||SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26),1)||SUBSTR(letras_placa,DBMS_RANDOM.VALUE(1,26),1);
+
+
+                                SELECT COUNT(*) INTO existe_placa
+                                FROM transportes
+                                WHERE numero_placa = placa;
+
+                                EXIT WHEN existe_placa = 0;
+
+                            END LOOP;
+                        END IF;
+
+                        --Condicional para determinar estatus del transporte
+                        IF CEIL(DBMS_RANDOM.VALUE(0,10)) <= 2 THEN
+                            estatus := 'd';
+                        ELSE
+                            estatus := 'f';
+                        END IF;
+
+                        /*Para cada estado se seleccionan n zonas aleatorias para insertar los transportes*/
+                        SELECT COUNT(*) INTO cantidad_zonas_proveedor
+                        FROM zonas_proveedores zp
+                        WHERE proveedor.ID=zp.ID_PROVEEDOR AND zp.id_estado = crear_estado.id;
+
+                        zona_transporte := CEIL(DBMS_RANDOM.VALUE(1,cantidad_zonas_proveedor));
+
+                        indice_zona := 1;
+
+                        FOR crear_zona IN (SELECT zp.ID_ZONA id_zona, zp.ID_MUNICIPIO id_municipio, zp.ID_ESTADO id_estado
+                                            FROM zonas_proveedores zp
+                                            WHERE proveedor.ID=zp.ID_PROVEEDOR AND zp.id_estado = crear_estado.id
+                                            ORDER BY DBMS_RANDOM.RANDOM())
+
+                        LOOP
+                            --Creacion del transporte
+                            IF indice_zona = zona_transporte THEN
+
+                                IF tipo_transporte = 'bic' THEN
+                                    INSERT INTO transportes (id, id_proveedor, id_estado, id_municipio, id_zona, tipo, estatus)
+                                    VALUES (id_transporte_sec.nextval, proveedor.ID, crear_zona.ID_ESTADO, crear_zona.ID_MUNICIPIO, crear_zona.ID_ZONA, tipo_transporte, estatus);
+                                ELSE
+                                    INSERT INTO transportes (id, id_proveedor, id_estado, id_municipio, id_zona, tipo, estatus, numero_placa)
+                                    VALUES (id_transporte_sec.nextval, proveedor.ID, crear_zona.ID_ESTADO, crear_zona.ID_MUNICIPIO, crear_zona.ID_ZONA, tipo_transporte, estatus, placa);
+
+                                END IF;
+
+                            END IF;
+
+                            indice_zona := indice_zona + 1;
+
+                        END LOOP;
+
+                    END LOOP;
+
 
                 END LOOP;
-
-            END LOOP;
 
         END LOOP;
 
