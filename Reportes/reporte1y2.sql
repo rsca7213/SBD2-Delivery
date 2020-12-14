9bf10deb06fdf8c8f9ba98df15c1d56b37e8e457
David Zacarias <davidjzacarias@gmail.com>
2020-12-14 15:36:14 -0400

    Detalle Reporte 2

--- Reportes/reporte1y2.sql
+++ Reportes/reporte1y2.sql
@@ -20,9 +20,11 @@ BEGIN
     SELECT pd.datos_empresa.logo AS Logo, TO_CHAR(pd.datos_empresa.fecha_registro,'DD/MM/YYYY') AS Fecha_Registro,
     pv.datos_empresa.nombre AS Proveedor, TO_CHAR(c.rango_fechas.fecha_inicio,'DD/MM/YYYY') AS Fehca_Desde,
     TO_CHAR(c.rango_fechas.fecha_fin,'DD/MM/YYYY') AS Fehca_Hasta, e.datos_ubicacion.nombre AS Estado,
-    (CASE WHEN c.descuento<100 THEN '$' || TO_CHAR(s.precio-s.precio*(c.descuento/100)) ELSE 'Exento de pago' END || ' ' ||
+    (CASE WHEN c.descuento<100 THEN '$' || TO_CHAR((s.precio-s.precio*(c.descuento/100))*ROUND(MONTHS_BETWEEN(c.rango_fechas.fecha_fin,c.rango_fechas.fecha_inicio))) ELSE 'Exento de pago' END || ' ' ||
     CASE WHEN s.frecuencia='d' THEN 'diario' WHEN s.frecuencia='s' THEN 'semanal' WHEN s.frecuencia='m' THEN 'mensual' ELSE 'anual' END) AS Monto
 	FROM productores pd, contratos c, proveedores pv, servicios s, servicios_contratos sc, estados e, estados_contratos ec
 	WHERE c.id_productor=pd.id AND s.id_proveedor=pv.id AND sc.id_contrato=c.id AND sc.id_productor=pd.id AND sc.id_proveedor=pv.id AND sc.id_servicio=s.id AND ((ec.id_estado=param_estado) OR (param_estado IS NULL)) AND ec.id_estado=e.id AND ec.id_contrato=c.id AND ec.id_productor=pd.id
 	ORDER BY pd.id, c.rango_fechas.fecha_inicio;
 END;
+
+
