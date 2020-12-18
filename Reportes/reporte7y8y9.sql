382a4e6ea9b8db5c9594c26c27d059c639a9d0e1
Ricardo Salvatorelli <rsca4321@gmail.com>
2020-12-18 17:54:59 -0400

    reporte 9 listo, query y jasper 🧾

--- Reportes/reporte7y8y9.sql
+++ Reportes/reporte7y8y9.sql
@@ -28,7 +28,7 @@ BEGIN
         ORDER BY e.datos_ubicacion.nombre, pr.datos_empresa.nombre, pe.tracking, u.email;
 END;
 
--- REPORTE 8S
+-- REPORTE 8
 CREATE OR REPLACE PROCEDURE reporte8 (ORACLE_REF_CURSOR OUT SYS_REFCURSOR, param_tracking IN INTEGER) IS
 BEGIN
     OPEN ORACLE_REF_CURSOR FOR
@@ -46,4 +46,30 @@ BEGIN
         INNER JOIN zonas z ON t.id_zona = z.id
         WHERE ((param_tracking IS NULL) OR (pe.tracking = param_tracking))
         AND pe.estatus != 'en';
+END;
+
+-- REPORTE 9
+CREATE OR REPLACE PROCEDURE reporte9 (ORACLE_REF_CURSOR OUT SYS_REFCURSOR, param_fecha_inicio IN DATE, param_fecha_fin IN DATE,
+param_estado IN INTEGER, param_zona IN INTEGER) IS
+BEGIN
+    OPEN ORACLE_REF_CURSOR FOR
+        SELECT NVL(TO_CHAR(param_fecha_inicio, 'dd/mm/yyyy'), 'Sin fecha') AS fecha_inicio,
+        NVL(TO_CHAR(param_fecha_fin, 'dd/mm/yyyy'), 'Sin fecha') AS fecha_fin,
+        e.datos_ubicacion.nombre AS estado, z.datos_ubicacion.nombre AS zona,
+        s.nombre AS sector, SUM(prpe.cantidad) AS cantidad
+        FROM productos_pedidos prpe
+        INNER JOIN productos pr ON pr.id = prpe.id_producto
+        INNER JOIN sectores s ON pr.id_sector = s.id
+        INNER JOIN pedidos pe ON pe.tracking = prpe.tracking_pedido
+        INNER JOIN zonas_usuarios zu ON pe.id_zona_destino = zu.id_zona
+        AND pe.id_municipio_destino = zu.id_municipio AND pe.id_estado_destino = zu.id_estado
+        AND pe.cedula_usuario = zu.cedula_usuario AND pe.id_proveedor_usuario = zu.id_proveedor_usuario
+        INNER JOIN zonas z ON z.id = zu.id_zona
+        INNER JOIN estados e ON e.id = zu.id_estado
+        WHERE ((param_estado IS NULL) OR (param_estado = e.id))
+        AND ((param_zona IS NULL) OR (param_zona = z.id))
+        AND ((param_fecha_inicio IS NULL) OR (TO_DATE(param_fecha_inicio) <= TRUNC(CAST(pe.rango_fechas.fecha_inicio AS DATE))))
+        AND ((param_fecha_fin IS NULL) OR (TO_DATE(param_fecha_fin) >= TRUNC(CAST(pe.rango_fechas.fecha_fin AS DATE))))
+        GROUP BY param_fecha_inicio, param_fecha_fin, e.datos_ubicacion.nombre, z.datos_ubicacion.nombre, s.nombre
+        ORDER BY e.datos_ubicacion.nombre, z.datos_ubicacion.nombre, s.nombre;
 END;
