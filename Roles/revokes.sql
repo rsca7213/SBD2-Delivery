d75c81dc18e2918327b1fc1ca97a3211ba106f99
Ricardo Salvatorelli <rsca4321@gmail.com>
2021-01-25 12:31:43 -0400

    fixes DDL seguridad

---
+++ Roles/revokes.sql
@@ -0,0 +1,23 @@
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+
+DROP USER admin1;
+DROP USER admin2;
+DROP USER admin3;
+DROP USER mcdonalds;
+DROP USER subway;
+DROP USER pizzahut;
+DROP USER alejandro;
+DROP USER jackson;
+DROP USER leonardo;
+DROP USER yummy;
+DROP USER ubigo;
+DROP USER delfoodve;
+DROP USER david;
+DROP USER ricardo;
+DROP USER jose;
+
+DROP ROLE administrador;
+DROP ROLE gerente_productor;
+DROP ROLE gerente_proveedor;
+DROP ROLE transportista;
+DROP ROLE usuario;
